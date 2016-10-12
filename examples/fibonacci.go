928838f0ebca4f52c3d6951a48e12c7a2eb77275
Edoardo Paolo Scalafiotti <edoardo849@gmail.com>
2016-10-12 13:26:23 +0100

    Presentation

---
+++ examples/fibonacci.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// START OMIT
+func main() {
+	c := make(chan int64)
+	defer close(c)
+
+	go fib(c, 1, 1)
+
+	for i := 0; i < 100; i++ {
+		x := <-c
+		fmt.Println(x)
+	}
+}
+
+func fib(c chan int64, a int64, b int64) {
+	c <- a
+	go fib(c, b, a+b)
+}
+
+// END OMIT
